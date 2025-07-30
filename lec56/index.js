56fb2742f67dbb2fcfad6d84fc8e89c98420be5f
Akash Singh <akashsinghrajput2724@gmail.com>
2025-07-30 17:19:53 +0530

    js learning

---
+++ lec56/index.js
@@ -0,0 +1,25 @@
+console.log("conditional tutorial")
+
+let age = 17;
+let grace=2;
+
+
+if ((age+grace) > 18) {
+    console.log("you can drive");
+} else {
+    console.log("you can't drive");
+}
+
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age%grace);
+
+console.log(age**grace);
+
+console.log(age++);
+
+console.log(age);
+
+console.log(++age);
